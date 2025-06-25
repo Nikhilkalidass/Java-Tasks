8073253120fa45cf183015434380cf7e851b2acc
Nikhilkalidass <nikhilkalidass0709@gmail.com>
2025-06-25 20:27:10 +0530

    Update EvenNumbers.java

--- EvenNumbers.java
+++ EvenNumbers.java
@@ -3,7 +3,7 @@ package Day1;
 public class EvenNumbers {
 public static void main(String[] args) {
 	System.out.println("Even numbers between 1 to 100:");
-	for(int i=0;i<=100;i +=2) {
+	for(int i=2;i<=100;i +=2) {
 		System.out.println(i+" ");
 	}
 }
