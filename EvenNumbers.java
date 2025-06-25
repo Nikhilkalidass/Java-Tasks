8a73e6d18c6afd4fccbc593461fa7ca098d43497
Nikhilkalidass <nikhilkalidass0709@gmail.com>
2025-06-25 20:25:03 +0530

    Add files via upload

---
+++ EvenNumbers.java
@@ -0,0 +1,10 @@
+package Day1;
+
+public class EvenNumbers {
+public static void main(String[] args) {
+	System.out.println("Even numbers between 1 to 100:");
+	for(int i=0;i<=100;i +=2) {
+		System.out.println(i+" ");
+	}
+}
+}
